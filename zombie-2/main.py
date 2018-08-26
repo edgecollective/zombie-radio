1cef7b291419d1487376f0a110bdd5ea4a96738d
Don Blair <donblair@gmail.com>
2018-08-26 18:35:02 -0400

    update

--- zombie-2/main.py
+++ zombie-2/main.py
@@ -4,17 +4,30 @@
 # This is achieved by running a "background service" (a coroutine)
 # and "pushing" the same event to each connected client.
 #
+
+TIMEOUT = 5
+DISPLAY = True
+
 import uasyncio
 import picoweb
 import network
 import time
+import gc
 
 from machine import Pin
 from machine import SPI
 
 from upy_rfm9x import RFM9x
 
-TIMEOUT = 5
+
+if DISPLAY:
+    import ssd1306
+    from machine import I2C
+    i2c = I2C(-1, Pin(22), Pin(23))
+    oled = ssd1306.SSD1306_I2C(128, 32, i2c)
+    oled.fill(0)
+    oled.show()
+
 
 sck=Pin(5)
 mosi=Pin(18)
@@ -118,21 +131,43 @@ def push_event(ev):
 
 def push_count():
     i = 0
+    free_mem_orig=0.
     while True:
+        if i==0:
+            free_mem_orig=gc.mem_free()
+            mem_diff=0
+        else:
+            mem_diff=gc.mem_free()-free_mem_orig
+        print("iter: %s; mem_diff = %s" % (i,mem_diff))
         rfm9x.receive(timeout=TIMEOUT)
         if rfm9x.packet is not None:
             packet_text = str(rfm9x.packet, 'ascii')
             rssi=str(rfm9x.rssi)
             print('Received: {0}'.format(packet_text))
             print("RSSI:",rssi)
+            if DISPLAY:
+                display_text="[%s]:%s,%s" % (i, packet_text,rssi)
+                update_display(display_text)
+            
             await push_event("[%s]: %s </br> RSSI: %s" % (i, packet_text,rssi))
         else:
+            if DISPLAY:
+                display_text="[%s]: (timed out)" % i
+                update_display(display_text)
             print("No packet ... ")
-            await push_event("[%s]: (timed out)" % i)
+            await push_event("[%s]:(timed out)" % i)
         i += 1
+        blink(.1)
+        gc.collect()
         await uasyncio.sleep(1)
 
 
+def update_display(display_text):
+    oled.fill(0)
+    oled.text(ip[0], 0, 0)
+    oled.text(':8081',0,10)
+    oled.text(display_text,0,20)
+    oled.show()
 #import logging
 #logging.basicConfig(level=logging.INFO)
 #logging.basicConfig(level=logging.DEBUG)
@@ -148,8 +183,10 @@ app = picoweb.WebApp(None, ROUTES)
 # 1 (True) debug logging
 # 2 extra debug logging
 print("host:"+ip[0])
+
+
 for i in range(3):
     blink(.2)
-led.value(1)
+
 # note: you'll need to visit ipaddress:8081
 app.run(debug=-1,host=ip[0])
