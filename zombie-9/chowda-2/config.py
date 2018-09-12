d176c01562d91e0e94366c5beaa49b3841fe4b22
Craig Wm. Versek <cversek@gmail.com>
2018-09-12 00:38:00 -0400

    restored DWB settings EXCEPT use_oled_display == False

--- zombie-9/chowda-2/config.py
+++ zombie-9/chowda-2/config.py
@@ -7,13 +7,13 @@ use_oled_display = False
 class radio:
     spi_id = 2
     baudrate = 5000000
-    sck  = Pin(5)
-    mosi = Pin(18)
-    miso = Pin(19)
-    cs   = Pin(12, Pin.OUT)
-    resetNum = 15
+    sck  = Pin(25)
+    mosi = Pin(33)
+    miso = Pin(32)
+    cs   = Pin(26, Pin.OUT)
+    resetNum = 27
     freq = 915.0
 
 class wifi:
-    ssid     = "dolphnet"
-    password = "mcmlxix1969"
+    ssid     = "zombie"
+    password = "disaster"
